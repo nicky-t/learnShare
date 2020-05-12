15be329ca1d0b96e3f0d832fd98c51efcafab056
EC2 Default User <ec2-user@ip-172-31-46-133.ap-northeast-1.compute.internal>
2020-05-12 13:37:33 +0000

    edit,updateアクション記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def show 
     @user = User.find(params[:id])
   end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  private
+  def user_params
+  end
 end
