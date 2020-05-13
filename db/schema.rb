6335ecb2f49c1b9d71da6771a2b695cbbde5fd98
EC2 Default User <ec2-user@ip-172-31-46-133.ap-northeast-1.compute.internal>
2020-05-13 12:59:40 +0000

    usersテーブルにgrup_id追加、マイグレート完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_124751) do
+ActiveRecord::Schema.define(version: 2020_05_13_125751) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2020_05_13_124751) do
     t.datetime "updated_at", null: false
     t.string "nickname"
     t.text "introduce"
+    t.integer "group_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
