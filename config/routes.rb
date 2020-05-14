86fbfcbae100e994733f538757511f86c75bcfd0
EC2 Default User <ec2-user@ip-172-31-46-133.ap-northeast-1.compute.internal>
2020-05-14 05:48:33 +0000

    groupsのshowアクションルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#index'
   resources :users, only: [:show, :edit, :update]
+  resources :groups, only: [:show]
 end
